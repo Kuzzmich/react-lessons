42573c9df790dbde1a59b019fb0542ecea52f892
Alexey Kramin <alexey.kramin@devhouse.pro>
2019-10-13 15:50:22 +0300

    init

---
+++ src/containers/App.js
@@ -0,0 +1,90 @@
+import React, {Component} from 'react'
+import classes from './App.module.css'
+import Persons from '../components/Persons/Persons'
+import Cockpit from '../components/Cockpit/Cockpit'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
+  // it's a more modern way to set state. otherwise you have ot init it in constructor
+  state = {
+    persons: [
+      {id: 'id1', name: 'Alex', age: 25},
+      {id: 'id2', name: 'Max', age: 20},
+      {id: 'id3', name: 'Henry', age: 33}
+    ],
+    showPersons: false
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
+  }
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id
+    })
+ч]
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+    person.name = event.target.value
+    const persons = [...this.state.persons]
+    persons[personIndex] = person
+
+    this.setState({
+      persons: persons
+    })
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons
+    this.setState({showPersons: !doesShow})
+  }
+
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice()
+    const persons = [...this.state.persons]
+    persons.splice(personIndex, 1)
+    this.setState({persons: persons})
+  }
+
+  render() {
+    console.log('[App.js] render');
+
+    let persons = null
+
+    if (this.state.showPersons) {
+      persons = <Persons
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangedHandler}
+          />
+    }
+
+    return (
+      <div className={classes.App}>
+        <Cockpit
+          title={this.props.appTitle}
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          clicked={this.togglePersonsHandler}
+        />
+
+        {persons}
+      </div>
+    );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m React!!'))
+  }
+}
+
+export default App
